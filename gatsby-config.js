75771ef249f3fe56412b26e44fe978d53868029f
Paul Williams <paul@paulswilliams.me.uk>
2018-02-18 18:39:31 +0000

    Listing and viewing title on blog pages. Also changed site title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Cheryl and Paul walk',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+    plugins: [
+        {
+            resolve: 'gatsby-source-contentful',
+            options: {
+                spaceId: 'r9yi0oycj9fa',
+                accessToken: 'b784f02e8c79269141e2e11ec717bae507945385f9a2fff18b1fbcb0025ca8a1',
+            },
+        },
+        'gatsby-plugin-react-helmet'],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,32 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-const IndexPage = () => (
-  <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </div>
+const BlogPost = ({ node }) => {
+    return (
+            <li>
+                <Link to={node.slug}>{node.title}</Link>
+            </li>
+    );
+}
+const IndexPage = ({ data }) => (
+        <ul>
+        {data.allContentfulBlog.edges.map((edge) => <BlogPost node={edge.node} />)}
+        </ul>
 )
 
 export default IndexPage
+
+export const pageQuery = graphql`
+    query pageQuery {
+        allContentfulBlog (filter: {
+            node_locale: {eq: "en-US"}
+        }) {
+            edges {
+                node {
+                    title
+                    slug
+                }
+            }
+        }
+    }
+`
