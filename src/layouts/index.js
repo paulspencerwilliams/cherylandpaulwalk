2024a249b69927a5538ca61473f1f5edb6afd6e3
Paul Williams <paul@paulswilliams.me.uk>
2018-02-21 19:31:41 +0000

    Contentful attribution

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -24,6 +24,9 @@ const TemplateWrapper = ({ children }) => (
       }}
     >
       {children()}
+      <a href='https://www.contentful.com/' rel='nofollow' target='_blank'>
+        <img src='https://images.contentful.com/fo9twyrwpveg/44baP9Gtm8qE2Umm8CQwQk/c43325463d1cb5db2ef97fca0788ea55/PoweredByContentful_LightBackground.svg' style={{maxWidth: '100px', width: '100%'}} alt='Powered by Contentful' />
+      </a>
     </div>
   </div>
 )
