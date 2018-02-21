a83d03c174d81f7fa9733b689f3260e6db98170e
Paul Williams <paul@paulswilliams.me.uk>
2018-02-21 18:17:28 +0000

    remove deleted access token from repo

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,8 +6,8 @@ module.exports = {
         {
             resolve: 'gatsby-source-contentful',
             options: {
-                spaceId: 'r9yi0oycj9fa',
-                accessToken: 'b784f02e8c79269141e2e11ec717bae507945385f9a2fff18b1fbcb0025ca8a1',
+                spaceId: process.env.CF_SPACE_ID,
+                accessToken: process.env.CF_ACCESS_TOKEN,
             },
         },
         'gatsby-transformer-remark',
